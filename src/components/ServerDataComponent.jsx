182a3741bb07343ef9aac0ebf5b9836d5780fc8f
Yur@Fiv3 <uraporskalo@gmail.com>
2024-12-30 14:24:09 +0200

    Fixing useEffect

--- src/components/ServerDataComponent.jsx
+++ src/components/ServerDataComponent.jsx
@@ -6,26 +6,21 @@ function ServerDataComponent() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(
-    () => {
-      const fetchData = async () => {
-        try {
-          const res = await axios.get(
-            'https://jsonplaceholder.typicode.com/posts'
-          );
-          setData(res.data);
-          setLoading(false);
-        } catch (error) {
-          setError(error.message);
-          setLoading(false);
-        }
-      };
-      fetchData();
-    },
-    setData,
-    setLoading,
-    setError
-  );
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          'https://jsonplaceholder.typicode.com/posts'
+        );
+        setData(res.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
